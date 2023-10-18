faef3cf5154cb891e40cf0e329913fd009f81d30
Discovery1701A <klettern.rosine0v@icloud.com>
2023-10-18 12:33:29 +0200

    6

--- Memory Kopie 4/Memory/EmojiMemoryGameView.swift
+++ Memory Kopie 4/Memory/EmojiMemoryGameView.swift
@@ -45,6 +45,7 @@ struct CardView: View {
                     shape.fill()
                     shape.foregroundColor(.white)
                     shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+                    Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 110-90)).padding(5).opacity(0.5)
                     Text (card.content).font(font (in: geometry.size))
                        
                 
@@ -61,7 +62,7 @@ struct CardView: View {
     private struct DrawingConstants {
         static let cornerRadius: CGFloat = 10
         static let lineWidth: CGFloat = 3
-        static let fontScale: CGFloat = 0.75
+        static let fontScale: CGFloat = 0.70
     }
 }
 
