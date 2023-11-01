040f2deff5c0b487104d41da64a84d9eb2e5c2e5
Discovery1701A <klettern.rosine0v@icloud.com>
2023-11-01 13:45:49 +0100

    2

---
+++ Set2/Set/AspectVGrid.swift
@@ -0,0 +1,91 @@
+//
+//  AspectVGrid.swift
+//  Set
+//
+//  Created by Anna Rieckmann on 19.10.23.
+//
+
+import Foundation
+import SwiftUI
+
+struct AspectVGrid<Item, ItemView>: View where ItemView: View, Item: Identifiable {
+    var items: [Item]
+    var aspectRatio: CGFloat
+    var content: (Item) -> ItemView
+    
+    
+    init (items: [Item], aspectRatio: CGFloat, @ViewBuilder content: @escaping (Item) -> ItemView) {
+        self.items = items
+        self.aspectRatio = aspectRatio
+        self.content = content
+    }
+    
+    var body: some View {
+        GeometryReader{ geometry in
+            VStack{
+                
+                let width = widthThatFitsWithMin(itemCount: items.count, in: geometry.size, itemAspectRatio: aspectRatio, minWidth: 70)
+                
+                ScrollView{
+                    weisauchnicht(width: width)
+                }
+                
+                //Text(String(Int(width)))
+            }
+        }
+    }
+    @ViewBuilder
+    func weisauchnicht(width : CGFloat)-> some View{
+        LazyVGrid(columns: [adaptiveGridItem(width: width)],spacing: 0) {
+            ForEach(items) { item in
+                content (item) .aspectRatio(aspectRatio, contentMode: .fit)
+            }
+        }
+    }
+    
+    
+    private func adaptiveGridItem(width : CGFloat)-> GridItem{
+        var gridItem = GridItem(.adaptive(minimum: width))
+        gridItem.spacing = 0
+        return gridItem
+    }
+    
+    private func widthThatFits(itemCount: Int, in size: CGSize, itemAspectRatio: CGFloat) -> CGFloat {
+        var columnCount = 1
+        var rowCount = itemCount
+        
+        repeat {
+            let itemWidth = size.width / CGFloat(columnCount)
+            let itemHeight = itemWidth / itemAspectRatio
+            if CGFloat (rowCount) * itemHeight < size.height {
+                break
+            }
+            
+            columnCount += 1
+            rowCount = (itemCount + (columnCount - 1)) / columnCount
+        }
+        
+        while columnCount < itemCount
+                if columnCount > itemCount {
+            columnCount = itemCount
+        }
+    
+        return floor(size.width / CGFloat (columnCount) )
+    }
+    
+    private func widthThatFitsWithMin(itemCount: Int, in size: CGSize, itemAspectRatio: CGFloat, minWidth: CGFloat) -> CGFloat {
+        var minWidthFit : CGFloat = minWidth
+        
+        for i in stride(from: 2 , through: itemCount-1, by: +3){
+            
+            if widthThatFits(itemCount: i, in: size, itemAspectRatio: itemAspectRatio) > minWidth {
+                minWidthFit = widthThatFits(itemCount: i, in: size, itemAspectRatio: itemAspectRatio)
+                // print("min",minWidthFit)
+            }else{
+                break
+            }
+        }
+        
+        return floor(minWidthFit)
+    }
+}
---
+++ Set2/Set/ViewModel.swift
@@ -0,0 +1,118 @@
+//
+//  viewModel.swift
+//  Set
+//
+//  Created by Anna Rieckmann on 19.10.23.
+//
+
+import SwiftUI
+
+
+class ViewModel: ObservableObject {
+    
+    typealias Card = SetGame<ContentShape, ContentColor, ContentPattern, NumberOfContentShapes>.Card
+    
+    static var cardContents: [Card.CardContent] = {
+        // geht alle möglichkeiten durch wegen enum und transformiert zu einem Array [1,0:]
+        return ContentShape.allCases.flatMap { shape in
+            ContentColor.allCases.flatMap { color in
+                ContentPattern.allCases.flatMap { pattern in
+                    NumberOfContentShapes.allCases.compactMap { numberOfShapes in
+                        Card.CardContent(shape: shape, color: color, pattern: pattern, numberOfShapes: numberOfShapes.rawValue)
+                    }
+                }
+            }
+        }.shuffled()
+    }() // Closure sollte ja drin sein
+    
+    private static func createSetGame () -> SetGame<ContentShape, ContentColor, ContentPattern, NumberOfContentShapes> {
+        SetGame(initialNumberOfPlayingCards: 12, totalNumberOfCards: cardContents.count) { cardContents[$0] }
+    }
+    
+    @Published private var model: SetGame<ContentShape, ContentColor, ContentPattern, NumberOfContentShapes> = createSetGame()
+    
+    var cards: Array<Card> {
+        return model.playingCards
+    }
+    
+    var allCards: Array<Card> {
+        return model.cards
+    }
+    
+    var numberOfPlayedCards: Int{
+        return model.numberOfPlayedCards
+    }
+    
+    var score : Int{
+        return model.score
+    }
+    var haveMatch: Bool{
+        return model.haveMatch
+    }
+    
+    var setAvailableInAllCards : Bool{
+        return model.setAvailableInAllCards
+    }
+    
+    var setAvailableInPlayedCards : Bool{
+        return model.setAvailableInPlayedCards
+    }
+    
+    // MARK: -Intent(s)
+    
+    func remove (){
+        model.remove()
+    }
+    
+    func choose (_ card: Card){
+        model.choose(card: card)
+    }
+    
+    func threeNewCards(){
+        model.threeNewCards()
+    }
+    
+    func newGame(){
+        model.newGame()
+    }
+    
+    func cheat(){
+        model.cheat()
+    }
+    
+    
+    enum ContentShape: CaseIterable {
+        case bean
+        case diamond
+        case rect
+    }
+    
+    enum ContentColor: CaseIterable {
+        case red
+        case green
+        case blue
+        
+        func getColor() -> Color {
+            switch self {
+            case .red:
+                return Color.red
+            case .green:
+                return Color.green
+            case .blue:
+                return Color.blue
+            }
+        }
+    }
+    
+    enum ContentPattern: CaseIterable {
+        case filled
+        case halftransperyt
+        case transpery
+    }
+    
+    enum NumberOfContentShapes: Int, CaseIterable {
+        case one = 1
+        case two = 2
+        case three = 3
+    }
+}
