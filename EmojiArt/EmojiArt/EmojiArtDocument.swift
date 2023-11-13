b25c84f7df36e2b1c6b618e772772fbcdd4aba19
Discovery1701A <klettern.rosine0v@icloud.com>
2023-11-13 11:09:08 +0100

    8mitte

--- EmojiArt/EmojiArt/EmojiArtDocument.swift
+++ EmojiArt/EmojiArt/EmojiArtDocument.swift
@@ -5,4 +5,42 @@
 //  Created by Anna Rieckmann on 13.11.23.
 //
 
-import Foundation
+import SwiftUI
+
+class EmojiArtDocument: ObservableObject
+{
+    
+    @Published private(set) var emojiArt: EmojiArtModel
+    init() {
+        emojiArt = EmojiArtModel ()
+    }
+    var emojis: [EmojiArtModel.Emoji] { emojiArt.emojis }
+    var background: EmojiArtModel.Background { emojiArt.background }
+    // MARK: - Intent(s)
+    func setBackground(_ background: EmojiArtModel.Background) {
+        emojiArt.background = background
+    }
+    
+    func addEmoji(_ text: String, at location: (x: Int, y: Int), size: CGFloat) {
+        emojiArt.addEmoji(text, at: location, size: Int(size))
+    }
+    
+    func moveEmoji(_ emoji: EmojiArtModel.Emoji, by offset: CGSize) {
+        if let index = emojiArt.emojis.index(matching: emoji) {
+            emojiArt.emojis[index].x += Int(offset.width)
+            emojiArt.emojis[index].y += Int(offset.height)
+        }
+    }
+    
+    func scaleEmoji(_ emoji: EmojiArtModel.Emoji, by scale: CGFloat) {
+        if let index = emojiArt.emojis.index(matching: emoji) {
+            emojiArt.emojis[index].size = Int((CGFloat(emojiArt.emojis[index].size) * scale).rounded(.toNearestOrAwayFromZero))
+        }
+    }
+    
+    func removeEmoji(_ emoji: EmojiArtModel.Emoji) {
+        if let index = emojiArt.emojis.index(matching: emoji) {
+            emojiArt.emojis.remove(at: index)
+        }
+    }
+}
